dcb4928fe2046ace77c40841be7ffff080f17833
maianoscuola <maiano.federico.21@itisriva.edu.it>
2025-02-19 16:10:26 +0100

    leggere il menu

--- src/volpintesta/test/HelloWorld.java
+++ src/volpintesta/test/HelloWorld.java
@@ -18,7 +18,34 @@ public class HelloWorld
     
     public static void main(String[] args) throws FileNotFoundException, IOException
     {
-        
+       FileReader reader = new FileReader("menu.txt");
+       
+        int scelta = 0;
+       System.out.println("1 per leggere il menu");
+       System.out.println("2 per dare una comanda");
+       System.out.println("3 per entrare in modalità amministratore");
+       System.out.println("4 per uscire");
+       scelta = Integer.parseInt(input.nextLine());
+        switch(scelta){
+           case 1:
+               int data = reader.read();
+               while(data != -1){
+                   System.out.print((char)data);
+                   data = reader.read();
+               }
+               break;
+               
+           case 2:
+               break;
+               
+           case 3:
+               break;
+               
+           case 4:
+               System.out.println("arrivederci");
+               break;
+               
+       } 
     }
     
 }
